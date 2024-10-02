a784c505105bebe8b59768f8f6fb088109e05bd8
elcadii <elcadi.mohammad.solicode@gmail.com>
2024-10-02 13:13:30 +0100

    Add files via upload

---
+++ pays/script.js
@@ -0,0 +1,28 @@
+
+let comteur = 1;
+function addlikes1() {
+    let outpute = `${comteur} likes` ;
+  document.getElementById("morocco-likes").innerHTML = outpute ;
+  comteur = comteur + 1 ;
+}
+
+
+let comteur1 = 1;
+function addlikes2() {
+    let outpute1 = `${comteur1} likes` ;
+  document.getElementById("usa-likes").innerHTML = outpute1;
+  comteur1 = comteur1 + 1 ;
+}
+let comteur2 = 1;
+function addlikes3() {
+    let outpute3 = `${comteur2} likes` ;
+  document.getElementById("sweden-likes").innerHTML = outpute3;
+  comteur2 = comteur2 + 1 ;
+}
+
+let comteur3 = 1;
+function addlikes4() {
+    let outpute4 = `${comteur3} likes` ;
+  document.getElementById("spain-likes").innerHTML = outpute4;
+  comteur3 = comteur3 + 1 ;
+}
